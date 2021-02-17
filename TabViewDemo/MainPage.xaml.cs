c2638889e1c3cd743451e9198f3df29101b12f78
Jonathan Eduardo Garcia Garcia <jonyjouna@gmail.com>
2021-02-16 21:10:01 -0600

    Binding SelectedIndex simple test SelectedIndex manually set Works with: https://github.com/Jouna77/XamarinCommunityToolkit

--- TabViewDemo/MainPage.xaml.cs
+++ TabViewDemo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace TabViewDemo
 {
@@ -12,6 +13,12 @@ namespace TabViewDemo
         private void TabViewItem_TabTapped(object sender, Xamarin.CommunityToolkit.UI.Views.TabTappedEventArgs e)
         {
             DisplayAlert("Title", "Item tapped", "Ok");
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);
+            this.TabView.SelectedIndex =Convert.ToInt32(btn.StyleId);
         }
     }
 }
